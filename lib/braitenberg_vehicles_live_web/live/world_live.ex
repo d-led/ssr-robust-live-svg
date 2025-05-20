dee7554c09ba42e44e151647decac809e2ea27d1
Dmitry Ledentsov <dmlled@yahoo.com>
2025-05-20 18:01:10 +0200

    just one circle

--- lib/braitenberg_vehicles_live_web/live/world_live.ex
+++ lib/braitenberg_vehicles_live_web/live/world_live.ex
@@ -7,6 +7,9 @@ defmodule BraitenbergVehiclesLiveWeb.WorldLive do
 
   def render(assigns) do
     ~H"""
+    <svg viewBox="0 0 800 600" width="800" height="600" style="border: 0.5px solid black;">
+      <circle cx="400" cy="300" r="20" fill="blue" />
+    </svg>
     """
   end
 end
